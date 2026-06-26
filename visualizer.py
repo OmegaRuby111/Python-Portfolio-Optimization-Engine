73656c4cea44c278f744e5ed64de05de63ded0b9
Sanka Skandhanuj <139116158+OmegaRuby111@users.noreply.github.com>
2026-06-26 16:52:38 +0530

    Update visualizer.py

--- visualizer.py
+++ visualizer.py
@@ -39,3 +39,51 @@ def plot_corr_heatmap(log_returns):
     ax.set_title("Asset Correlation Heatmap")
     plt.tight_layout()
     return fig
+
+def plot_max_drawdown(returns,spy_returns):
+    fig,axes=plt.subplots(2,1,figsize=(8,6))
+    returns_history=np.exp(returns.cumsum())
+    spy_returns_history=np.exp(spy_returns.cumsum())
+    drawdown_history=[]
+    spy_drawdown_history=[]
+    running_max=returns_history.iloc[0]
+    spy_running_max=spy_returns_history.iloc[0]
+    for i in returns_history:
+        if running_max<=i:
+            running_max=i
+        drawdown=(i-running_max)/running_max
+        drawdown_history.append(drawdown)
+    for i in spy_returns_history:
+        if spy_running_max<=i:
+            spy_running_max=i
+        spy_drawdown=(i-spy_running_max)/spy_running_max
+        spy_drawdown_history.append(spy_drawdown)
+    drawdown_series=pd.Series(drawdown_history, index=returns_history.index)
+    spy_drawdown_series=pd.Series(spy_drawdown_history, index=spy_returns_history.index)
+    axes[0].plot(drawdown_series)
+    axes[1].plot(spy_drawdown_series)
+    axes[0].set_xlabel("Dates")
+    axes[0].set_ylabel("Drawdown")
+    axes[0].set_title("Portfolio Drawdown") 
+    axes[1].set_xlabel("Dates")
+    axes[1].set_ylabel("SPY Drawdown")
+    axes[1].set_title("SPY Drawdown")
+    return fig
+
+def plot_rolling_sharpe(returns,spy_returns,window):
+    fig,axes=plt.subplots(2,1,figsize=(8,6))
+    rolling_mean=returns.rolling(window).mean()
+    rolling_std=returns.rolling(window).std()
+    rolling_sharpe=rolling_mean/rolling_std
+    spy_rolling_mean=spy_returns.rolling(window).mean()
+    spy_rolling_std=spy_returns.rolling(window).std()
+    spy_rolling_sharpe=spy_rolling_mean/spy_rolling_std
+    axes[0].set_xlabel("Dates")
+    axes[0].set_ylabel("Sharpe Ratio")
+    axes[0].set_title("Rolling Sharpe Ratio")
+    axes[0].plot(rolling_sharpe)
+    axes[1].set_xlabel("Dates")
+    axes[1].set_ylabel("SPY Sharpe Ratio")
+    axes[1].set_title("SPY Rolling Sharpe Ratio")
+    axes[1].plot(spy_rolling_sharpe)
+    return fig
